e912f64756ceef542ecc60f333751f9a2e48fcd7
4drian3d <adriangonzalesval@gmail.com>
2022-12-17 15:33:31 -0500

    Improved Plugin Logic

---
+++ src/main/java/me/feusalamander/vmessage/Listeners.java
@@ -0,0 +1,107 @@
+package me.feusalamander.vmessage;
+
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.connection.DisconnectEvent;
+import com.velocitypowered.api.event.connection.PostLoginEvent;
+import com.velocitypowered.api.event.player.PlayerChatEvent;
+import com.velocitypowered.api.event.player.ServerConnectedEvent;
+import com.velocitypowered.api.proxy.Player;
+import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.proxy.ServerConnection;
+import com.velocitypowered.api.proxy.server.RegisteredServer;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.LuckPermsProvider;
+import net.luckperms.api.model.user.User;
+
+import java.util.Objects;
+
+public final class Listeners {
+    private static final LegacyComponentSerializer SERIALIZER = LegacyComponentSerializer.builder()
+            .character('&')
+            .hexColors()
+            .build();
+    private LuckPerms luckPermsAPI;
+    private final Configuration configuration;
+    private final ProxyServer proxyServer;
+
+    Listeners(ProxyServer proxyServer, Configuration configuration) {
+        if (proxyServer.getPluginManager().getPlugin("luckperms").isPresent()){
+            this.luckPermsAPI = LuckPermsProvider.get();
+        }
+        this.configuration = configuration;
+        this.proxyServer = proxyServer;
+    }
+
+    @Subscribe
+    private void onMessage(PlayerChatEvent e){
+        if (!configuration.isMessageEnabled()) {
+            return;
+        }
+        Player p = e.getPlayer();
+        String m = e.getMessage();
+        String message = configuration.getMessageFormat()
+                .replace("#player#", p.getUsername())
+                .replace("#message#", m)
+                .replace("#server#", p.getCurrentServer().orElseThrow().getServerInfo().getName());
+        if (luckPermsAPI != null){
+            User user = luckPermsAPI.getPlayerAdapter(Player.class).getUser(p);
+            message = message.replace("#prefix#", Objects.requireNonNull(user.getCachedData().getMetaData().getPrefix()));
+        }
+        final String finalMessage = message;
+        proxyServer.getAllServers().forEach(server -> {
+            if (!Objects.equals(p.getCurrentServer().map(ServerConnection::getServerInfo).orElse(null), server.getServerInfo())){
+                server.sendMessage(SERIALIZER.deserialize(finalMessage));
+            }
+        });
+    }
+
+    @Subscribe
+    private void onJoin(PostLoginEvent e){
+        if (!configuration.isJoinEnabled()) {
+            return;
+        }
+        Player p = e.getPlayer();
+        String message = configuration.getJoinFormat().replace("#player#", p.getUsername());
+        if (luckPermsAPI != null){
+            User user = luckPermsAPI.getPlayerAdapter(Player.class).getUser(p);
+            message = message.replace("#prefix#", Objects.requireNonNull(user.getCachedData().getMetaData().getPrefix()));
+        }
+        proxyServer.sendMessage(SERIALIZER.deserialize(message));
+    }
+
+    @Subscribe
+    private void onLeave(DisconnectEvent e){
+        if (!configuration.isLeaveEnabled()) {
+            return;
+        }
+        Player p = e.getPlayer();
+        String message = configuration.getLeaveFormat()
+                .replace("#player#", p.getUsername());
+        if (luckPermsAPI != null){
+            User user = luckPermsAPI.getPlayerAdapter(Player.class).getUser(p);
+            message = message.replace("#prefix#", Objects.requireNonNull(user.getCachedData().getMetaData().getPrefix()));
+        }
+        proxyServer.sendMessage(SERIALIZER.deserialize(message));
+    }
+
+    @Subscribe
+    private void onChange(ServerConnectedEvent e){
+        if (!configuration.isChangeEnabled()) {
+            return;
+        }
+        e.getPreviousServer().ifPresent(server -> {
+            RegisteredServer actual = e.getServer();
+            Player p = e.getPlayer();
+            String message = configuration.getChangeFormat()
+                    .replace("#player#", p.getUsername())
+                    .replace("#oldserver#", server.getServerInfo().getName())
+                    .replace("#server#", actual.getServerInfo().getName());
+            if (luckPermsAPI != null){
+                User user = luckPermsAPI.getPlayerAdapter(Player.class).getUser(p);
+                message = message.replace("#prefix#", Objects.requireNonNull(user.getCachedData().getMetaData().getPrefix()));
+            }
+            proxyServer.sendMessage(SERIALIZER.deserialize(message));
+        });
+    }
+}
---
+++ src/main/java/me/feusalamander/vmessage/VMessage.java
@@ -0,0 +1,47 @@
+package me.feusalamander.vmessage;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.plugin.Dependency;
+import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
+import com.velocitypowered.api.proxy.ProxyServer;
+import org.slf4j.Logger;
+import java.nio.file.Path;
+
+@Plugin(
+        id = "vmessage",
+        name = "Vmessage",
+        version = "1.2",
+        description = "A velocity plugin that creates a multi server chat for the network",
+        authors = {"FeuSalamander"},
+        dependencies = { @Dependency(id = "luckperms", optional = true) }
+)
+public class VMessage {
+    private final ProxyServer proxy;
+    private final Logger logger;
+    private final Metrics.Factory metricsFactory;
+    private final Path dataDirectory;
+
+    @Inject
+    public VMessage(ProxyServer proxy, Logger logger, Metrics.Factory metricsFactory, @DataDirectory Path dataDirectory) {
+        this.proxy = proxy;
+        this.logger = logger;
+        this.metricsFactory = metricsFactory;
+        this.dataDirectory = dataDirectory;
+    }
+
+    @Subscribe
+    private void onProxyInitialization(ProxyInitializeEvent event) {
+        Configuration configuration = Configuration.load(dataDirectory);
+
+        if (configuration == null) {
+            return;
+        }
+
+        metricsFactory.make(this, 16527);
+        proxy.getEventManager().register(this, new Listeners(proxy, configuration));
+        logger.info("Vmessage by FeuSalamander is working !");
+    }
+}
